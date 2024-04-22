4264f792b47adadbcaeacd96bcdf95bfc07d1591
VncsDionizio <167775868+VncsDionizio@users.noreply.github.com>
2024-04-22 17:39:02 -0300

    Update Robo_ThabVini.ino

--- Robo_ThabVini.ino
+++ Robo_ThabVini.ino
@@ -1,12 +1,13 @@
-#define ENB 2 
-#define esqF 3 
+#define ENB 3 
+#define esqF 2 
 #define esqT 4
-#define dirF 5 
-#define dirT 6 
-#define ENA 7
+#define dirF 6 
+#define dirT 7 
+#define ENA 5
 #define sensorD 8
 #define sensorC 9
 #define sensorE 10
+//preto = 1 , branco = 0
 
 bool estadoD, estadoC, estadoE;
 
@@ -27,6 +28,39 @@ void loop() {
 estadoD = digitalRead(sensorD);
 estadoC = digitalRead(sensorC);
 estadoE = digitalRead(sensorE); 
+
+if (estadoE == 0 & estadoC == 1 & estadoD == 0) {
+frente();
+}
+
+else if (estadoE == 1 & estadoC == 0 & estadoD == 0) {
+esquerda();
+}
+
+else if (estadoE == 1 & estadoC == 1 & estadoD == 0) {
+devagar_esquerda();
+}
+
+else if (estadoE == 0 & estadoC == 0 & estadoD == 1) {
+direita();
+}
+
+else if (estadoE == 0 & estadoC == 1 & estadoD == 1) {
+devagar_direita();
+}
+
+else if (estadoE == 0 & estadoC == 0 & estadoD == 0) {
+devagar();
+}
+
+else if (estadoE == 1 & estadoC == 0 & estadoD == 1) {
+devagar();
+}
+
+else{
+  parar();
+}
+
 Serial.print("D");
 Serial.print(estadoD);
 Serial.print(" C");

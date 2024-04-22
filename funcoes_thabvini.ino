801093ef9d1be2d03513c39be29f4efb3d61a37f
VncsDionizio <167775868+VncsDionizio@users.noreply.github.com>
2024-04-22 17:38:37 -0300

    Update funcoes_thabvini.ino

--- funcoes_thabvini.ino
+++ funcoes_thabvini.ino
@@ -33,6 +33,7 @@ void esquerda (){
   analogWrite (ENB, 185 ); //esquerda
   analogWrite (ENA, 185 ); //direita
 }
+
 void devagar (){
   digitalWrite(dirF, HIGH);
   digitalWrite(dirT, LOW);
@@ -41,3 +42,30 @@ void devagar (){
   analogWrite (ENB, 150 ); //esquerda
   analogWrite (ENA, 150 ); //direita
 } 
+
+void parar(){
+  digitalWrite(dirF, LOW);
+  digitalWrite(dirT, LOW);
+  digitalWrite(esqF, LOW);
+  digitalWrite(esqT, LOW);
+  analogWrite (ENB, 150 ); //esquerda
+  analogWrite (ENA, 150 ); //direita
+}
+
+void devagar_direita (){
+  digitalWrite(dirF, HIGH);
+  digitalWrite(dirT, LOW);
+  digitalWrite(esqF, HIGH);
+  digitalWrite(esqT, LOW);
+  analogWrite (ENB, 185 ); //esquerda
+  analogWrite (ENA, 150 ); //direita
+}
+
+void devagar_esquerda (){
+  digitalWrite(dirF, HIGH);
+  digitalWrite(dirT, LOW);
+  digitalWrite(esqF, HIGH);
+  digitalWrite(esqT, LOW );
+  analogWrite (ENB, 150 ); //esquerda
+  analogWrite (ENA, 185 ); //direita
+}
